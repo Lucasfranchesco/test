e24967f69e4ef4f839c004969d29c94b14d25c2e
Lucasfranchesco <153239784+Lucasfranchesco@users.noreply.github.com>
2023-12-14 20:59:46 -0300

    Update lucardo V3 main.py
    is the V3

--- lucardo main.py
+++ lucardo main.py
@@ -1,2 +1,12 @@
-nombre = input("Inserta tu nombre")
-print("hola mi amig@"),nombre,",(bienvenido)
+import random
+password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+long_contra = int(input("escribe la longitud de la contraseña"))
+
+generada = ""
+
+
+for i in range(long_contra):
+    generada += random.choice(password)
+
+print(generada)    
